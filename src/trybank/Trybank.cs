5272a622d68cb6570a9b555bad1b0486d4e85709
Enio Schaefer <enio100schaefer@hotmail.com>
2023-07-12 17:54:44 -0300

    feat: Req. 03

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -69,7 +69,15 @@
     // 3. Construa a funcionalidade de fazer Logout
     public void Logout()
     {
-      throw new NotImplementedException();
+      if (Logged)
+      {
+        Logged = false;
+        loggedUser = -99;
+      }
+      else
+      {
+        throw new AccessViolationException("Usuário não está logado");
+      }
     }
 
     // 4. Construa a funcionalidade de checar o saldo
